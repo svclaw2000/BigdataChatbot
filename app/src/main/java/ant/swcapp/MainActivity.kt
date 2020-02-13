a76d240124a96db898d3ca3eb78777b8bce11b54
K.H.Park <svclaw2000@gmail.com>
2020-02-13 15:54:22 +0900

    Update gradle and migrate to androidx

--- app/src/main/java/ant/swcapp/MainActivity.kt
+++ app/src/main/java/ant/swcapp/MainActivity.kt
@@ -6,13 +6,13 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.AudioAttributes
 import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
